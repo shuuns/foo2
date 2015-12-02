3067681c6b65b39ca4873faf1f0dce620f00688a
eclipse@flex14 <shuuns@gmail.com>
2015-12-02 23:30:00 +0800

    add base sql commands

--- BeAngulr2/src/js/controllers/sqlite.js
+++ BeAngulr2/src/js/controllers/sqlite.js
@@ -1,16 +1,71 @@
 ﻿app.controller('cwmoneyCtrl', function ($scope, sqliteFactory) {
     console.log('cwmoney #1')
     getDBcallback = function (db) {
-        var sqlite_master = db.exec("SELECT `name`, `sql` FROM `sqlite_master` WHERE type='table';");
+        var contents = db.exec(getAccs());
+        console.log(contents);
+        $scope.columns = contents[0].columns;
+        $scope.values = contents[0].values;
+        
+        //var str_sql = groupByMonth(2015,11,1);
+        //var contents = db.exec(getKinds());
         //console.log(contents);
-        $scope.columns = sqlite_master[0].columns;
-        $scope.values = sqlite_master[0].values;
+        //console.log(new Date(2015,11,1).getTime() / 1000);
     };
     //load db
-    sqliteFactory.getDB('api/2013_05_21_CHT.iDB', getDBcallback);
+    sqliteFactory.getDB('api/2015_12_01_CHT.iDB', getDBcallback);
 });
 
-app.controller('cwmDateCtrl', function ($scope) {
+function cwDate(date){
+	return date/1000;
+};
+
+function sumByMonth (y, m, type){
+	var sql;
+	sql = "select sum(i_money) from rec_table where i_date between " + cwDate(new Date(y, m - 1, 1)) + " and " + cwDate(new Date(y, m, 0)) + " and i_type=" + type + ";"; 
+	return sql;
+};
+
+function groupByMonth (y, m, type){
+	var sql;
+	sql = "select i_kind, sum(i_money) from rec_table where i_date between " + cwDate(new Date(y, m - 1, 1)) + " and " + cwDate(new Date(y, m, 0)) + " and i_type=" + type + " group by i_kind;"; 
+	return sql;
+};
+
+function groupNameByMonth (y, m, type){
+	var sql;
+	
+//	select kindtext, a.i_kind,  i_money from (
+//	  select i_kind, sum(i_money) as i_money from rec_table where i_date between 1446307200 and 1448812800 and i_type=1 group by i_kind order by i_kind) a, kind_table b
+//	where a.i_kind=b._id
+			
+	sql = "select i_kind, sum(i_money) from rec_table where i_date between " + cwDate(new Date(y, m - 1, 1)) + " and " + cwDate(new Date(y, m, 0)) + " and i_type=" + type + " group by i_kind"; 
+	return sql;
+};
+//支出分類
+function getKind(){
+	return "select _id, kindtext from kind_table;";
+};
+//支出子分類
+function getKinds(){
+	return "select _id, kindid, kindstext from kinds_table;";
+};
+//收入分類
+function getInKind(){
+	return "select _id, kindtext from in_kind_table;";
+};
+//收入子分類
+function getInKinds(){
+	return "select _id, kindid, kindstext from in_kinds_table;";
+};
+//accounts
+function getAccs(){
+	return "select _id, acctext, accrate, accnote, accmoney, accsort, accinit, rev1, rev2, rev3 from acc_table;";
+};
+//sqlite_master
+function getMaster() {
+	return "SELECT `name`, `sql` FROM `sqlite_master` WHERE type='table';";
+};
+app.controller('cwmDateCtrl', function ($scope, $filter) {
     $scope.open = function ($event) {
         $event.preventDefault();
         $event.stopPropagation();
@@ -18,8 +73,9 @@ app.controller('cwmDateCtrl', function ($scope) {
         $scope.opened = true;
     };
 
-    $scope.dt = new Date();
-
+    $scope.dt = $filter('date')(new Date(),'yyyy-MM-dd');
+    $scope.dt2 = null;
+    
 
 });
 
--- BeAngulr2/src/js/main.js
+++ BeAngulr2/src/js/main.js
@@ -12,7 +12,7 @@ angular.module('app')
 
       // config
       $scope.app = {
-        name: 'AngulrX',
+        name: 'Angulr',
         version: '2.0.0',
         // for chart colors
         color: {
